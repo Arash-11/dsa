519a4fab8db1fdcf37eee59dde519bbf961df19f
Arash-11 <arashnawy@gmail.com>
2024-10-04 00:25:02 -0400

    add binary search tree

---
+++ tree/bst.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+} Node;
+
+Node* get_new_node(int data) {
+    Node* new_node = malloc(sizeof(Node));
+    new_node->data = data;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    return new_node;
+}
+
+void insert(Node** rootptr, int data) {
+    if (*rootptr == NULL) {
+        *rootptr = get_new_node(data);
+        return;
+    }
+
+    if (data <= (*rootptr)->data) {
+        insert(&((*rootptr)->left), data);
+        return;
+    }
+
+    insert(&((*rootptr)->right), data);
+}
+
+bool search(Node* root, int data) {
+    if (root == NULL) return false;
+
+    if (data == root->data) return true;
+
+    if (data < root->data)
+        return search(root->left, data);
+
+    return search(root->right, data);
+}
+
+int main() {
+    Node* root = NULL; // create an empty tree
+
+    insert(&root, 15);
+    insert(&root, 10);
+    insert(&root, 20);
+    insert(&root, 25);
+    insert(&root, 8);
+    insert(&root, 12);
+
+    printf("20: %d\n", search(root, 20));
+    printf("8: %d\n", search(root, 8));
+    printf("0: %d\n", search(root, 0));
+    printf("11: %d\n", search(root, 11));
+
+    return 0;
+}
